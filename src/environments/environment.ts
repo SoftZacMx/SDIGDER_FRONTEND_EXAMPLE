23a597006ffd54d2589cc4c6378a26926175cce0
Brayan <brayan@MacBook-Air-de-Brayan.local>
2025-08-14 19:30:00 -0600

    Changing production URL

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,34 +2,34 @@
 export const environment = {
   API_URSL: {
       API_AUTH: {
-        LOGIN:'https://sidgder-api-demo.up.railway.app/api/auth/login',
-        RESET_PASSWORD:'https://sidgder-api-demo.up.railway.app/api/auth/set-password',
-        VERIFY_USER:'https://sidgder-api-demo.up.railway.app/api/auth/verify_user'
+        LOGIN:'https://sidgder-api-demo.onrender.com/api/auth/login',
+        RESET_PASSWORD:'https://sidgder-api-demo.onrender.com/api/auth/set-password',
+        VERIFY_USER:'https://sidgder-api-demo.onrender.com/api/auth/verify_user'
       },
       API_USERS: {
-        USERS:'https://sidgder-api-demo.up.railway.app/api/users',
+        USERS:'https://sidgder-api-demo.onrender.com/api/users',
       },
-      API_SAOURCES:'https://sidgder-api-demo.up.railway.app/api/saources',
-      API_SAOURCES_CATEGORIES:'https://sidgder-api-demo.up.railway.app/api/saources-categories',
-      API_ORDERS:'https://sidgder-api-demo.up.railway.app/api/orders',
-      API_CASHS_REGISTER:'https://sidgder-api-demo.up.railway.app/api/cash_register',
-      API_CASHS_OPERATIONS:'https://sidgder-api-demo.up.railway.app/api/cash_register_operations',
-      API_REPORTS:'https://sidgder-api-demo.up.railway.app/api/reports',
-      API_ORDERS_REPORTS:'https://sidgder-api-demo.up.railway.app/api/orders_reports',
+      API_SAOURCES:'https://sidgder-api-demo.onrender.com/api/saources',
+      API_SAOURCES_CATEGORIES:'https://sidgder-api-demo.onrender.com/api/saources-categories',
+      API_ORDERS:'https://sidgder-api-demo.onrender.com/api/orders',
+      API_CASHS_REGISTER:'https://sidgder-api-demo.onrender.com/api/cash_register',
+      API_CASHS_OPERATIONS:'https://sidgder-api-demo.onrender.com/api/cash_register_operations',
+      API_REPORTS:'https://sidgder-api-demo.onrender.com/api/reports',
+      API_ORDERS_REPORTS:'https://sidgder-api-demo.onrender.com/api/orders_reports',
 
-      API_BUSSINES_SERVICES:'https://sidgder-api-demo.up.railway.app/api/bussines_services',
-      API_BUSSINES_SERVICES_PAYMENTS:'https://sidgder-api-demo.up.railway.app/api/bussines_services_payments',
-      API_EMPLOYEES_SALARIES_PAYMENTS:'https://sidgder-api-demo.up.railway.app/api/employees_salaries',
-      API_MERCHANDISE_PURCHASE:'https://sidgder-api-demo.up.railway.app/api/purchase_merchandise',
+      API_BUSSINES_SERVICES:'https://sidgder-api-demo.onrender.com/api/bussines_services',
+      API_BUSSINES_SERVICES_PAYMENTS:'https://sidgder-api-demo.onrender.com/api/bussines_services_payments',
+      API_EMPLOYEES_SALARIES_PAYMENTS:'https://sidgder-api-demo.onrender.com/api/employees_salaries',
+      API_MERCHANDISE_PURCHASE:'https://sidgder-api-demo.onrender.com/api/purchase_merchandise',
 
-      API_TABLES:'https://sidgder-api-demo.up.railway.app/api/tables',
+      API_TABLES:'https://sidgder-api-demo.onrender.com/api/tables',
 
-      API_PRODUCTS:'https://sidgder-api-demo.up.railway.app/api/products',
-      API_PRODUCTS_CATEGORIES:'https://sidgder-api-demo.up.railway.app/api/products/categories',
+      API_PRODUCTS:'https://sidgder-api-demo.onrender.com/api/products',
+      API_PRODUCTS_CATEGORIES:'https://sidgder-api-demo.onrender.com/api/products/categories',
 
 
     },
-    MENU_URL:'https://deliyunos.up.railway.app/public/menu',
+    MENU_URL:'https://sidgder-api-demo.onrender.com/public/menu',
 }
 
  
